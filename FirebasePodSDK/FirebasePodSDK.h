4c5b7c36ee2086510ac2b34bbf67dfbfd8f5a9ba
iMad <damianos.imad@gmail.com>
2023-07-15 01:43:19 +0300

    Initial Commit

---
+++ FirebasePodSDK/FirebasePodSDK.h
@@ -0,0 +1,18 @@
+//
+//  FirebasePodSDK.h
+//  FirebasePodSDK
+//
+//  Created by iMad on 15/07/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FirebasePodSDK.
+FOUNDATION_EXPORT double FirebasePodSDKVersionNumber;
+
+//! Project version string for FirebasePodSDK.
+FOUNDATION_EXPORT const unsigned char FirebasePodSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FirebasePodSDK/PublicHeader.h>
+
+
